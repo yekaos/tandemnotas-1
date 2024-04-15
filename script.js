e66065bf7a61ef53e4efed84aca795f680fe7b2c
Crisbm9 <cristinab.tandem@gmail.com>
2024-04-15 09:55:11 +0200

    notitas

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
-//PASOS
-//recoger la informacion
-//almacenar la info en localStorage
-//leerla
-//borrarla
-
+document.addEventListener('DOMContentLoaded',function(){
+  showInfo();
+})
+document.getElementById('btn-add-nota').addEventListener('click', function(){
+  getInfo(nota)
+  setLocalInfo(nota)
+  showInfo()
+})
+document.getElementById('btn-clean-notas').addEventListener('click', function(){
+  cleanInfo()
+  showInfo()
+})
+// recoger la información
 function getInfo(){
-  //devuelve el texto
-  console.log('texto ingresado')
-  return 'texto ingresado'
+  // capturamos lo que el usuario introduce
+  let nota = document.getElementById('nota').innerHTML
+  console.log(nota)
+  return nota
 }
-
-//almacenar en localStore
-function setLocalInfo(){
-  //guardar la info en localStorage
-  return true;
+// alamacenarla en localStorage
+function setLocalInfo(nota){
+  // guardaría la info en localStorage
+  const clave = Date.now();
+localStorage.setItem(clave,getInfo(nota))
 }
-
-//leer y sacarla por pantalla
+// leerla y sacarla por pantalla
 function showInfo(){
-  //leerla y mostrarla por pantalla
-  //si no hay notas mostrar no hay nada
-  return true;
+  // leerla y mostrarla por pantalla
+  //  sino hay notas mostra no hay nada
+  // limpiamos la pantalla
+  document.getElementById('show-notas').innerHTML= ''
+  for (let index = 0; index < localStorage.length; index++) {
+      let clave = localStorage.key(index)
+      let valor = localStorage[clave]
+      console.log(valor)
+      let elemento = `
+      <div class='lista-notas'>
+      ${valor}
+      </div>
+      `
+      document.getElementById('show-notas').innerHTML+= elemento
+  }
 }
-
-//limpiarla
-function clearInfo(){
-  //limpiar el local storage y la pantalla
+// limpiarla
+function cleanInfo(){
+  // limpiar el local storage y limpiar la pantaal
+  // Elimina todos los elementos
+localStorage.clear();
+  console.log('En localstorage no hay notas')
 }
-
-
 
 
 
